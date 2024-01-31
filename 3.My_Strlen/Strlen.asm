5236ed4119a5eff872304a8e9b695736f2f8d0a0
Cédric Roulof <114474418+shookx@users.noreply.github.com>
2024-01-31 11:19:30 +0400

    Add files via upload

--- 3.My_Strlen/Strlen.asm
+++ 3.My_Strlen/Strlen.asm
@@ -4,4 +4,14 @@ section .text
     global my_Strlen
 
 my_Strlen:
-        
+	xor rcx, rcx
+
+loop_strlen:
+	cmp byte[rdi + rcx], 0
+	je end_strlen
+	inc rcx
+	jmp loop_strlen
+
+end_strlen:
+	mov rax, rcx
+	ret
