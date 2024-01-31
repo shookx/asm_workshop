e40198396602b5d05341fe78777d81a7688577cb
Cédric Roulof <114474418+shookx@users.noreply.github.com>
2024-01-31 11:19:04 +0400

    Add files via upload

--- 2.My_Hello_World/HelloWorld.asm
+++ 2.My_Hello_World/HelloWorld.asm
@@ -9,3 +9,15 @@ section .text
     global _start
 
 _start:
+	mov rsi ,hello
+	mov rdi ,1
+	mov rax ,4
+	mov rdx ,len_hello
+	mov ebx, 0
+	mov eax, 1
+	syscall
+
+	int 0x80
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
