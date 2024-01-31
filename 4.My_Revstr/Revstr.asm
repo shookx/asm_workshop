6fe2d31410778e59cb44fc7fec66d9b16d55953d
Cédric Roulof <114474418+shookx@users.noreply.github.com>
2024-01-31 11:19:50 +0400

    Add files via upload

--- 4.My_Revstr/Revstr.asm
+++ 4.My_Revstr/Revstr.asm
@@ -4,5 +4,31 @@ section .text
     global my_Revstr
 
 my_Revstr:
+	init_rev:
+		xor r8, r8
+		xor rcx, rcx
+		xor rax, rax
+		jmp rev_loop_push
 
+	rev_loop_push:
+		mov r8b, [rdi + rcx]
+		cmp r8b, 0
+		jne rev_loop_push_continue
+		jmp rev_loop_pop
 
+	rev_loop_push_continue:
+		push r8
+		inc rcx
+		jmp rev_loop_push
+
+	rev_loop_pop:
+		pop r8
+		mov [rdi  + rax], r8
+		inc rax
+		dec rcx
+		cmp rcx, 0
+		jne rev_loop_pop
+		jmp end_rev
+
+	end_rev:
+		ret
